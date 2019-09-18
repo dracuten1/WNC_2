a75e7b248b3c5c993dfad22b30e20177a890f655
dracuten1 <tuyennguyenleminh@gmail.com>
2019-09-18 18:32:14 +0700

    Updates

---
+++ precache-manifest.9652fbca10a9ec869846142715734958.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f00f7cdf5f2416acd27442c2e1afe5",
+    "url": "/WNC_2/index.html"
+  },
+  {
+    "revision": "6de5437996f13da3766b",
+    "url": "/WNC_2/static/css/main.6d58631e.chunk.css"
+  },
+  {
+    "revision": "3cf034578ed0dc53cff3",
+    "url": "/WNC_2/static/js/2.c42504a1.chunk.js"
+  },
+  {
+    "revision": "6de5437996f13da3766b",
+    "url": "/WNC_2/static/js/main.ea0ee618.chunk.js"
+  },
+  {
+    "revision": "8157ec231b3f02b45062",
+    "url": "/WNC_2/static/js/runtime~main.6ff7502b.js"
+  }
+]);
